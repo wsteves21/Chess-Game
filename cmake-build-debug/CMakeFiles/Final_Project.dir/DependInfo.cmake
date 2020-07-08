51ea936161a7422774ffb29e46fa903d2998fbd1
wsteves21 <32177809+wsteves21@users.noreply.github.com>
2020-07-07 19:20:25 -0500

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/Final_Project.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/waynesteven/Documents/ECE_4122/ECE4122_FinalProject-master/Bishop.cpp" "/Users/waynesteven/Documents/ECE_4122/ECE4122_FinalProject-master/cmake-build-debug/CMakeFiles/Final_Project.dir/Bishop.cpp.o"
+  "/Users/waynesteven/Documents/ECE_4122/ECE4122_FinalProject-master/King.cpp" "/Users/waynesteven/Documents/ECE_4122/ECE4122_FinalProject-master/cmake-build-debug/CMakeFiles/Final_Project.dir/King.cpp.o"
+  "/Users/waynesteven/Documents/ECE_4122/ECE4122_FinalProject-master/Knight.cpp" "/Users/waynesteven/Documents/ECE_4122/ECE4122_FinalProject-master/cmake-build-debug/CMakeFiles/Final_Project.dir/Knight.cpp.o"
+  "/Users/waynesteven/Documents/ECE_4122/ECE4122_FinalProject-master/Pawn.cpp" "/Users/waynesteven/Documents/ECE_4122/ECE4122_FinalProject-master/cmake-build-debug/CMakeFiles/Final_Project.dir/Pawn.cpp.o"
+  "/Users/waynesteven/Documents/ECE_4122/ECE4122_FinalProject-master/Piece.cc" "/Users/waynesteven/Documents/ECE_4122/ECE4122_FinalProject-master/cmake-build-debug/CMakeFiles/Final_Project.dir/Piece.cc.o"
+  "/Users/waynesteven/Documents/ECE_4122/ECE4122_FinalProject-master/Queen.cpp" "/Users/waynesteven/Documents/ECE_4122/ECE4122_FinalProject-master/cmake-build-debug/CMakeFiles/Final_Project.dir/Queen.cpp.o"
+  "/Users/waynesteven/Documents/ECE_4122/ECE4122_FinalProject-master/Rook.cpp" "/Users/waynesteven/Documents/ECE_4122/ECE4122_FinalProject-master/cmake-build-debug/CMakeFiles/Final_Project.dir/Rook.cpp.o"
+  "/Users/waynesteven/Documents/ECE_4122/ECE4122_FinalProject-master/Square.cpp" "/Users/waynesteven/Documents/ECE_4122/ECE4122_FinalProject-master/cmake-build-debug/CMakeFiles/Final_Project.dir/Square.cpp.o"
+  "/Users/waynesteven/Documents/ECE_4122/ECE4122_FinalProject-master/main.cc" "/Users/waynesteven/Documents/ECE_4122/ECE4122_FinalProject-master/cmake-build-debug/CMakeFiles/Final_Project.dir/main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Final_Project.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Final_Project.dir/main.cc.o"
+  "CMakeFiles/Final_Project.dir/Piece.cc.o"
+  "CMakeFiles/Final_Project.dir/Square.cpp.o"
+  "CMakeFiles/Final_Project.dir/Knight.cpp.o"
+  "CMakeFiles/Final_Project.dir/Bishop.cpp.o"
+  "CMakeFiles/Final_Project.dir/Queen.cpp.o"
+  "CMakeFiles/Final_Project.dir/King.cpp.o"
+  "CMakeFiles/Final_Project.dir/Pawn.cpp.o"
+  "CMakeFiles/Final_Project.dir/Rook.cpp.o"
+  "Final_Project.pdb"
+  "Final_Project"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Final_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
